3993850a29a582cdf74afd0ddb4627c014ecf4f8
amjadcp <amjadcp03@gmail.com>
2023-03-14 18:06:34 +0530

    add:login

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,5 +5,6 @@ const router = require("express").Router();
 
 
 router.post("/login-admin", login(RoleEnum.ADMIN))
+router.post("/login", login([RoleEnum.ALUMINI, RoleEnum.STUDENT]))
 
 module.exports = router
